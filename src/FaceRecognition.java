ff2f9b632de83c532ca1f4bcac035949084c777b
ecen <ericgul@student.chalmers.se>
2019-02-26 14:09:16 +0100

    Added truePositive.

--- src/FaceRecognition.java
+++ src/FaceRecognition.java
@@ -26,10 +26,12 @@ public class FaceRecognition {
     }
 
     private static class PerformanceStats {
+        public double truePositive;
         public double falsePositive;
         public double falseNegative;
 
-        public PerformanceStats(double falsePositive, double falseNegative){
+        public PerformanceStats(double truePositive, double falsePositive, double falseNegative){
+            this.truePositive = truePositive;
             this.falsePositive = falsePositive;
             this.falseNegative = falseNegative;
         }
@@ -264,7 +266,8 @@ public class FaceRecognition {
         }
         double falseNegative = (double)nrWrongIsFace/(nrCorrectIsFace+ nrWrongIsFace);
         double falsePositive = (double)nrWrongIsNotFace/(nrCorrectIsNotFace+nrWrongIsNotFace);
-        return new PerformanceStats(falsePositive, falseNegative);
+        double truePositive = (double)nrCorrectIsFace/(nrCorrectIsFace+nrWrongIsFace);
+        return new PerformanceStats(truePositive, falsePositive, falseNegative);
     }
 
     public static boolean isFace(ArrayList<Classifier> degenerateDecisionTree, HalIntegralImage i) throws Exception{

aa253a18f1647548783f0224a633384da479c3f7
ecen <ericgul@student.chalmers.se>
2019-03-06 22:15:30 +0100

    Data loading now normalizes percentages. Added variables for controlling maximum set size.

--- src/Data.java
+++ src/Data.java
@@ -3,96 +3,52 @@ import Catalano.Imaging.FastBitmap;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Data {
 
     public String[] pathsToFaces;
     public String[] pathsToNonFaces;
 
+    // Percentages and maximums for datasets.
+    // Percentages are always normalized to 1.
     public double percentTrainFaces = 0.8;
-    public double percentTrainNonFaces = 0.3;
+    public int maxTrainFaces = 4000;
+    public double percentTrainNonFaces = 0.1;
+    public int maxTrainNonFaces = 4000;
+
     public double percentTestFaces = 0.1;
-    public double percentTestNonFaces = 0.1;
+    public int maxTestFaces = 30000;
+    public double percentTestNonFaces = 0.7;
+    public int maxTestNonFaces = 30000;
+
     public double percentValidateFaces = 0.1;
+    public int maxValidateFaces = 400;
     public double percentValidateNonFaces = 0.1;
-
-    String originalTrainFaces = "./res/faces/original-train-face";
-    String originalTestFaces = "./res/faces/original-test-face";
-    String attFaces = "./res/faces/att";
-    String lfw2Faces = "./res/faces/lfw2-19px";
-
-    String originalTrainNonFaces = "./res/non-faces/original-test-non-face";
-    String originalTestNonFaces =  "./res/non-faces/original-train-non-face";
-    String crawledNonFaces = "./res/non-faces/scraped";
-
-
-    String manyFacesTest = "./test-res/examples";
-
+    public int maxValidateNonFaces = 2000;
     public List<LabeledIntegralImage> negativeSamples;
     public List<LabeledIntegralImage> positiveSamples;
     public List<LabeledIntegralImage> allSamples;
-
     public List<LabeledIntegralImage> testData;
     public List<LabeledIntegralImage> validationData;
+    String originalTrainFaces = "./res/faces/original-train-face";
+    String originalTestFaces = "./res/faces/original-test-face";
+    String attFaces = "./res/faces/att";
+    String lfw2Faces = "./res/faces/lfw2-19px"; // For testing only
+    String fddbFaces = "./res/faces/fddb-flat"; // For testing only
+    String originalTrainNonFaces = "./res/non-faces/original-test-non-face";
+    String originalTestNonFaces = "./res/non-faces/original-train-non-face";
+    String crawledNonFaces = "./res/non-faces/scraped";
+    String manyFacesTest = "./test-res/examples";
 
     public Data() throws Exception {
-        pathsToFaces = new String[]{originalTrainFaces, attFaces};
-        pathsToNonFaces = new String[]{originalTrainNonFaces, originalTestNonFaces, crawledNonFaces};
+        //pathsToFaces = new String[]{originalTrainFaces, attFaces};
+        //pathsToNonFaces = new String[]{originalTrainNonFaces, originalTestNonFaces, crawledNonFaces};
+        pathsToFaces = new String[]{lfw2Faces};
+        pathsToNonFaces = new String[]{originalTestNonFaces};
 
-        // Read images from file system amd calculate integralImages.
-        // This now uses our own HalIntegralImage. It seems to work, but there could be bugs.
-        ArrayList<HalIntegralImage> faces = new ArrayList<>();
-        ArrayList<HalIntegralImage> nonFaces = new ArrayList<>();
-        try {
-            // Read images for faces
-            for(String path : pathsToFaces) faces.addAll(Data.readImagesFromDataBase(path));
-             // Read images for non-faces
-            for(String path : pathsToNonFaces) nonFaces.addAll(Data.readImagesFromDataBase(path));
+        partitionData();
 
-        } catch (Exception e) {
-            System.err.println("Data folder not found. Have you extracted data.zip correctly?");
-            System.exit(1);
-        }
-        // Re-store arrays of training data as a list and add face label.
-        double weightFace = 1.0 / (2 * faces.size());
-        double weightNoFace = 1.0 / (2 * nonFaces.size());
-
-        ArrayList<LabeledIntegralImage> allFaces = new ArrayList<>();
-        ArrayList<LabeledIntegralImage> allNonFaces = new ArrayList<>();
-
-        for(HalIntegralImage img : faces) allFaces.add(new LabeledIntegralImage(img, true, weightFace));
-        for(HalIntegralImage img : nonFaces) allNonFaces.add(new LabeledIntegralImage(img, false, weightNoFace));
-
-        Collections.shuffle(allFaces);
-        Collections.shuffle(allNonFaces);
-
-        int endTrainFacesIndex = (int)((allFaces.size()-1)*percentTrainFaces);
-        int endTrainNonFacesIndex = (int)((allNonFaces.size()-1)*percentTrainNonFaces);
-        int endValidateFacesIndex = (int)((allFaces.size()-1)*(percentTrainFaces+percentValidateFaces));
-        int endValidateNonFacesIndex = (int)((allNonFaces.size()-1)*(percentTrainNonFaces+percentValidateNonFaces));
-        int endTestFacesIndex = (int)((allFaces.size()-1)*(percentTrainFaces+percentValidateFaces+percentTestFaces));
-        int endTestNonFacesIndex = (int)((allNonFaces.size()-1)*(percentTrainNonFaces+percentValidateNonFaces+percentTestNonFaces));
-
-        if(endTestFacesIndex >= allFaces.size() || endTestNonFacesIndex >= allNonFaces.size()) {
-            throw new Exception("The final index is larger than allFaces or allNonFaces, perhaps the sum of percentages exceed 1.");
-        }
-
-        positiveSamples = new ArrayList<>(allFaces.subList(0, endTrainFacesIndex));
-        negativeSamples = new ArrayList<>(allNonFaces.subList(0, endTrainNonFacesIndex));
-
-        allSamples = new ArrayList<>();
-        allSamples.addAll(negativeSamples);
-        allSamples.addAll(positiveSamples);
-
-        validationData = new ArrayList<>(allFaces.subList(endTrainFacesIndex+1, endValidateFacesIndex));
-        validationData.addAll(allNonFaces.subList(endTrainNonFacesIndex+1, endValidateNonFacesIndex));
-
-        testData = new ArrayList<>(allFaces.subList(endValidateFacesIndex+1, endTestFacesIndex));
-        testData.addAll(allNonFaces.subList(endValidateNonFacesIndex+1, endTestNonFacesIndex));
 
         // Pre-calculate all feature values
         System.out.println("Pre-calculating feature values for training data...");
@@ -102,8 +58,80 @@ public class Data {
 
     }
 
+    private void partitionData() throws Exception {
+        // Read images from file system amd calculate integralImages.
+        // This now uses our own HalIntegralImage. It seems to work, but there could be bugs.
+        ArrayList<HalIntegralImage> faces = new ArrayList<>();
+        ArrayList<HalIntegralImage> nonFaces = new ArrayList<>();
+        try {
+            // Read images for faces
+            for (String path : pathsToFaces) faces.addAll(Data.readImagesFromDataBase(path));
+            // Read images for non-faces
+            for (String path : pathsToNonFaces) nonFaces.addAll(Data.readImagesFromDataBase(path));
+
+        } catch (Exception e) {
+            System.err.println("Data folder not found. Have you extracted res.zip correctly?");
+            System.exit(1);
+        }
+
+        ArrayList<LabeledIntegralImage> allFaces = new ArrayList<>();
+        ArrayList<LabeledIntegralImage> allNonFaces = new ArrayList<>();
+
+        for (HalIntegralImage img : faces) allFaces.add(new LabeledIntegralImage(img, true, 0));
+        for (HalIntegralImage img : nonFaces) allNonFaces.add(new LabeledIntegralImage(img, false, 0));
+
+        Collections.shuffle(allFaces, new Random(1));
+        Collections.shuffle(allNonFaces, new Random(1));
+
+        double totalFacePercentage = percentTrainFaces + percentValidateFaces + percentTestFaces;
+        double totalNonFacePercentage = percentTrainNonFaces + percentValidateNonFaces + percentTestNonFaces;
+
+        int endTrainFacesIndex = (int) ((allFaces.size() - 1) * (percentTrainFaces / totalFacePercentage));
+        int endTrainNonFacesIndex = (int) ((allNonFaces.size() - 1) * (percentTrainNonFaces / totalNonFacePercentage));
+
+        int endValidateFacesIndex = (int) ((allFaces.size() - 1) * (percentTrainFaces + percentValidateFaces) / totalFacePercentage);
+        int endValidateNonFacesIndex = (int) ((allNonFaces.size() - 1) * (percentTrainNonFaces + percentValidateNonFaces) / totalNonFacePercentage);
+
+        int endTestFacesIndex = allFaces.size();
+        int endTestNonFacesIndex = allNonFaces.size();
+
+        positiveSamples = new ArrayList<>(allFaces.subList(0, Math.min(endTrainFacesIndex, maxTestFaces)));
+        negativeSamples = new ArrayList<>(allNonFaces.subList(0, Math.min(endTrainNonFacesIndex, maxTestNonFaces)));
+
+        // Set training data weights
+        double weightFace = 1.0 / (2 * positiveSamples.size());
+        double weightNoFace = 1.0 / (2 * negativeSamples.size());
+
+        for (LabeledIntegralImage i : positiveSamples) {
+            i.setWeight(weightFace);
+        }
+        for (LabeledIntegralImage i : negativeSamples) {
+            i.setWeight(weightNoFace);
+        }
+
+        allSamples = new ArrayList<>();
+        allSamples.addAll(negativeSamples);
+        allSamples.addAll(positiveSamples);
+
+        validationData = new ArrayList<>(
+                allFaces.subList(endTrainFacesIndex,
+                        Math.min(endValidateFacesIndex, endTrainFacesIndex + maxValidateFaces)));
+        validationData.addAll(allNonFaces.subList(
+                endTrainNonFacesIndex,
+                Math.min(endValidateNonFacesIndex, endTrainNonFacesIndex + maxValidateNonFaces)));
+
+        testData = new ArrayList<>(allFaces.subList(
+                endValidateFacesIndex,
+                Math.min(endTestFacesIndex, endValidateFacesIndex + maxTestFaces)));
+        testData.addAll(allNonFaces.subList(
+                endValidateNonFacesIndex,
+                Math.min(endTestNonFacesIndex, endValidateNonFacesIndex + maxTestFaces)));
+
+    }
+
     /**
      * Reads the images from
+     *
      * @param path a path to a folder containing only images. Images should have correct size and other properties.
      * @throws IOException
      */
@@ -130,12 +158,13 @@ public class Data {
 
     /**
      * Removes the data that does not pass the strong classifier.
+     *
      * @param cascadeClassifier
      * @param data
      * @return an array of images that the strong classifier thinks could be faces
      */
     public static ArrayList<LabeledIntegralImage> filter(CascadeClassifier cascadeClassifier, List<LabeledIntegralImage> data) throws Exception {
-        ArrayList<LabeledIntegralImage> maybeFaces = new ArrayList<>(data.size()/2);
+        ArrayList<LabeledIntegralImage> maybeFaces = new ArrayList<>(data.size() / 2);
         for (LabeledIntegralImage d : data) {
             if (cascadeClassifier.isFace(d.img)) {
                 maybeFaces.add(d);
@@ -167,7 +196,7 @@ public class Data {
         return s;
     }
 
-    public static void saveStrong(StrongClassifier strongClassifier, String fileName){
+    public static void saveStrong(StrongClassifier strongClassifier, String fileName) {
         save(strongClassifier, fileName);
     }
 
